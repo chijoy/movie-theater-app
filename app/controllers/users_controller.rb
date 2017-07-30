b81346338732cbdfd0c2f52bc00c99d48d105058
Joy <joymccaffreychicago@gmail.com>
2017-07-30 14:33:07 -0500

    Add actions in user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,61 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+                            movie_id: params[:movie_id],
+                            movietheater_id: params[:movietheater_id],
+                            first_name: params[:first_name],
+                            last_name: params[:last_name],
+                            email: params[:email],
+                            text_option: params[:text_option],
+                            birthdate: params[:birthdate],
+                            type: params[:type]
+                            )
+    if @user.save
+      flash[:success] = "You successfully created an account!"
+      redirect_to "/"
+    else
+      @user.errors.full_messages
+      render 'new.html.erb'
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+                    movie_id: params[:movie_id],
+                    movietheater_id: params[:movietheater_id],
+                    first_name: params[:first_name],
+                    last_name: params[:last_name],
+                    email: params[:email],
+                    text_option: params[:text_option],
+                    birthdate: params[:birthdate],
+                    type: params[:type]
+                    )
+    flash[:success] = "Your account information has been updated."
+    redirect_to "/"
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+
+    flash[:success] = "Your account was deleted. :("
+    redirect_to '/'
+  end
 end
