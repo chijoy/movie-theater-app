f3d2e589bf4a24910b18c250d61d5e955096156b
Joy <joymccaffreychicago@gmail.com>
2017-07-30 14:06:09 -0500

    Add sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,22 @@
 class SessionsController < ApplicationController
-  
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      flash[:success] = "You're in!"
+      redirect_to '/'
+    else
+      flash[:warning] = 'Something went terribly wrong'
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Buh bye"
+    redirect_to '/login'
+  end
 end
