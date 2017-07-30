a522dbbcb411a4f90aaefb2b69ca9456165ff44b
Joy <joymccaffreychicago@gmail.com>
2017-07-30 15:58:58 -0500

    Add new and create to movietheater controller to create the movietheater.

--- app/controllers/movietheaters_controller.rb
+++ app/controllers/movietheaters_controller.rb
@@ -1,6 +1,14 @@
 class MovietheatersController < ApplicationController
   # This is the client, the actual movie complex
-  def index
-    
+  def index    
+  end
+
+  def new
+  end
+
+  def create
+    @movietheater = Movietheater.create(
+                                        name: params[:name]
+                                        )
   end
 end
