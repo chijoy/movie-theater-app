26cdb2d52d2d3a105855ac1b6e363e3337da361e
Joy <joymccaffreychicago@gmail.com>
2017-07-30 19:40:57 -0500

    Add seed data for seats for each auditorium.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,20 @@
 # Auditorium.create(movietheater_id: 1)
 
 # Seats belong to an auditorium
-Auditorium.create(movietheater_id: 1)
+# 10.times do
+#   Seat.create(
+#               auditorium_id: 1,
+#               )
+# end
 
+# 20.times do
+#   Seat.create(
+#               auditorium_id: 2,
+#               )
+# end
 
-CategoryProduct.create!([
-  {category_id: 1, product_id: 2},
-  {category_id: 1, product_id: 4},
-  {category_id: 1, product_id: 7},
-  {category_id: 2, product_id: 1},
-  {category_id: 2, product_id: 2},
-  {category_id: 2, product_id: 4},
-  {category_id: 2, product_id: 7},
-  {category_id: 3, product_id: 5},
-  {category_id: 3, product_id: 6},
-  {category_id: 1, product_id: 3},
-  {category_id: 1, product_id: 5},
-  {category_id: 2, product_id: 5},
-  {category_id: 2, product_id: 6}
-
-  200.times do
-  Address.create(
-                address_1: Faker::Address.street_address,
-                address_2: Faker::Address.secondary_address,
-                city: Faker::Address.city,
-                state: Faker::Address.state,
-                zip: Faker::Address.zip,
-                employee_id: Employee.all.sample.id
-    )
-end
+# 30.times do
+#   Seat.create(
+#               auditorium_id: 3,
+#               )
+# end
