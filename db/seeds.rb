3e3b3f2c38afadc9365518e91d316df9fa08cab6
Joy <joymccaffreychicago@gmail.com>
2017-07-30 19:26:13 -0500

    Add seed data for auditoriums.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+# The Movietheater is the movie complex.
 # Movietheater.create(name: "Chicago Movie Theater")
+
+# Auditoriums are the individual theaters with the movietheater complex.
+# Auditorium.create(movietheater_id: 1)
